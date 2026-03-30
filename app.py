ebe7599ad265f0dd0f22d3c3dad5b75d127fae65
Dhruvin28 <dhruvinmashruwala28@gmail.com>
2026-03-30 20:27:47 +0530

    fix

--- app.py
+++ app.py
@@ -166,4 +166,5 @@ def download_zip(job_id: str):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
